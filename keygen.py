f829a3b36c7831262c7bfb0824d20307162de215
qqiumax <maxqqqq@hotmail.com>
2022-06-06 11:07:13 +0800

    bugfix+content

--- keygen.py
+++ keygen.py
@@ -3,7 +3,6 @@ import hashlib
 def keygen():
     s = random.randint(2**165, (2**166)-1)
     k = input("type a pwd: ")
-    k = "asd"
     k = int(hashlib.sha1(k.encode("utf-8")).hexdigest(),16)
     p = int(s/k)
     so = open("key.sec", "w" , encoding="utf-8")
